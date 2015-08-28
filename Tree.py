f28d98bdef806da93de4dbbd682c3fb6d3203b27
cor <cor@pruijs.nl>
2015-08-28 13:19:00 +0200

    Add tree generating script

---
+++ Tree.py
@@ -0,0 +1,26 @@
+import sys
+
+tree_width = 25
+
+if len(sys.argv) == 2:
+	tree_width = int(sys.argv[1])
+
+def print_tree(size):
+	# Get the string for every line of the tree	
+	def get_component(component_number):
+		# Return None on every even number
+		if component_number % 2 == 0:
+			return None
+		else:
+			# Line is odd, print line
+			side_width = (size - component_number) / 2
+			return "-" * side_width + "#" * component_number + "-" * side_width
+
+	# Loop through the lines and print the line
+	for i in range(tree_width + 1):
+		if get_component(i) is not None:
+			print get_component(i)
+	# Print the base
+	print get_component(1);
+
+print_tree(tree_width)
